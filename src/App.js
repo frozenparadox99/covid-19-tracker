313a9c9ec9ef6faead122e8da0aea837f532842e
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-12 14:30:56 +0530

    added country picker

--- src/App.js
+++ src/App.js
@@ -3,24 +3,37 @@ import React from "react";
 import { Cards, CountryPicker, Chart } from "./components";
 
 import styles from "./App.module.css";
+import image from "./images/image.png";
 
 import { fetchData } from "./api";
 
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    // Fetch the data
+    const fetchedData = await fetchData(country);
+
+    // Set the state
+
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
